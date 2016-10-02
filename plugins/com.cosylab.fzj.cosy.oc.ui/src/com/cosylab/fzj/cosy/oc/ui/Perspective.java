e8ddd15ec0a10f0efd3d0c386abb2550d93fbeac
jbobnar <jaka.bobnar@cosylab.com>
2016-10-02 13:20:39 +0200

    Perspective updated
    Removed editor from the layout, added view to perspective, relative to
    the editorss.

--- plugins/com.cosylab.fzj.cosy.oc.ui/src/com/cosylab/fzj/cosy/oc/ui/Perspective.java
+++ plugins/com.cosylab.fzj.cosy.oc.ui/src/com/cosylab/fzj/cosy/oc/ui/Perspective.java
@@ -7,6 +7,6 @@ public class Perspective implements IPerspectiveFactory {
 
     @Override
     public void createInitialLayout(IPageLayout layout) {
-        // TODO Auto-generated method stub
+        layout.setEditorAreaVisible(false);
     }
 }
