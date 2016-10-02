a9a21babdd1d586d57e6c32076572014f5bb2cfd
jbobnar <jaka.bobnar@cosylab.com>
2016-10-02 15:13:16 +0200

    Created the class.

---
+++ plugins/com.cosylab.fzj.cosy.oc.ui/src/com/cosylab/fzj/cosy/oc/ui/TooltipCheckBox.java
@@ -0,0 +1,26 @@
+package com.cosylab.fzj.cosy.oc.ui;
+
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Tooltip;
+
+/**
+ * <code>TooltipCheckBox</code> is a version of checkbox which provides the tooltip identical to the text of the check
+ * box.
+ *
+ * @author <a href="mailto:jaka.bobnar@cosylab.com">Jaka Bobnar</a>
+ *
+ */
+public class TooltipCheckBox extends CheckBox {
+
+    /**
+     * Construct a new check box with the given text and tooltip.
+     * 
+     * @param text
+     *            the text to use as the checkbox text and tooltip
+     */
+    public TooltipCheckBox(String text) {
+        super(text);
+        setTooltip(new Tooltip(text));
+    }
+
+}
