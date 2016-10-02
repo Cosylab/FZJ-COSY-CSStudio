b8923e2f811d485cf7009030901f1fdc6ebe3c81
jbobnar <jaka.bobnar@cosylab.com>
2016-10-02 15:12:00 +0200

    Changed all ObjectPoperty<Double> to DoubleProperty.

--- plugins/com.cosylab.fzj.cosy.oc.ui/src/com/cosylab/fzj/cosy/oc/ui/LatticeElementChartValues.java
+++ plugins/com.cosylab.fzj.cosy.oc.ui/src/com/cosylab/fzj/cosy/oc/ui/LatticeElementChartValues.java
@@ -2,49 +2,51 @@ package com.cosylab.fzj.cosy.oc.ui;
 
 import com.cosylab.fzj.cosy.oc.LatticeElementType;
 
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
 public class LatticeElementChartValues {
 
     private ObjectProperty<LatticeElementType> elementType = new SimpleObjectProperty<>(this, "elementType");
-    private ObjectProperty<Double> positionValue = new SimpleObjectProperty<>(this, "positionValue");
-    private ObjectProperty<Double> horizontalOrbitValue = new SimpleObjectProperty<>(this, "horizontalOrbitValue");
-    private ObjectProperty<Double> verticalOrbitValue = new SimpleObjectProperty<>(this, "verticalOrbitValue");
-    private ObjectProperty<Double> goldenHorizontalOrbitValue = new SimpleObjectProperty<>(this, "goldenHorizontalOrbitValue");
-    private ObjectProperty<Double> goldenVerticalOrbitValue = new SimpleObjectProperty<>(this, "goldenVerticalOrbitValue");
-    private ObjectProperty<Double> horizontalCorrectionValue = new SimpleObjectProperty<>(this, "horizontalCorrectionValue");
-    private ObjectProperty<Double> verticalCorrectionValue = new SimpleObjectProperty<>(this, "verticalCorrectionValue");
+    private DoubleProperty positionValue = new SimpleDoubleProperty(this, "positionValue");
+    private DoubleProperty horizontalOrbitValue = new SimpleDoubleProperty(this, "horizontalOrbitValue");
+    private DoubleProperty verticalOrbitValue = new SimpleDoubleProperty(this, "verticalOrbitValue");
+    private DoubleProperty goldenHorizontalOrbitValue = new SimpleDoubleProperty(this, "goldenHorizontalOrbitValue");
+    private DoubleProperty goldenVerticalOrbitValue = new SimpleDoubleProperty(this, "goldenVerticalOrbitValue");
+    private DoubleProperty horizontalCorrectionValue = new SimpleDoubleProperty(this, "horizontalCorrectionValue");
+    private DoubleProperty verticalCorrectionValue = new SimpleDoubleProperty(this, "verticalCorrectionValue");
 
     public ObjectProperty<LatticeElementType> getElementType() {
         return elementType;
     }
 
-    public ObjectProperty<Double> getPositionValue() {
+    public DoubleProperty getPositionValue() {
         return positionValue;
     }
 
-    public ObjectProperty<Double> getHorizontalOrbitValue() {
+    public DoubleProperty getHorizontalOrbitValue() {
         return horizontalOrbitValue;
     }
 
-    public ObjectProperty<Double> getVerticalOrbitValue() {
+    public DoubleProperty getVerticalOrbitValue() {
         return verticalOrbitValue;
     }
 
-    public ObjectProperty<Double> getGoldenHorizontalOrbitValue() {
+    public DoubleProperty getGoldenHorizontalOrbitValue() {
         return goldenHorizontalOrbitValue;
     }
 
-    public ObjectProperty<Double> getGoldenVerticalOrbitValue() {
+    public DoubleProperty getGoldenVerticalOrbitValue() {
         return goldenVerticalOrbitValue;
     }
 
-    public ObjectProperty<Double> getHorizontalCorrectionValue() {
+    public DoubleProperty getHorizontalCorrectionValue() {
         return horizontalCorrectionValue;
     }
 
-    public ObjectProperty<Double> getVerticalCorrectionValue() {
+    public DoubleProperty getVerticalCorrectionValue() {
         return verticalCorrectionValue;
     }
 }
