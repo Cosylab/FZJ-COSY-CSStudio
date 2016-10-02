f3a4126770e08e503a3ceaa6d7ed833b21d1bd71
jbobnar <jaka.bobnar@cosylab.com>
2016-10-02 15:11:27 +0200

    Performance improvements.

--- plugins/com.cosylab.fzj.cosy.oc.ui/src/com/cosylab/fzj/cosy/oc/ui/CorrectionsChart.java
+++ plugins/com.cosylab.fzj.cosy.oc.ui/src/com/cosylab/fzj/cosy/oc/ui/CorrectionsChart.java
@@ -17,27 +17,29 @@ public class CorrectionsChart<X,Y> extends LineChart<X,Y> {
         if (getData() == null) {
             return;
         }
+        Axis<X> xAxis = getXAxis();
+        Axis<Y> yAxis = getYAxis();
+        final double yAxisHeight = yAxis.getHeight();
+        final double yAxisZeroPos = yAxis.getZeroPosition();
+        final boolean isYAxisZeroPosVisible = !Double.isNaN(yAxisZeroPos);
+        
         for (Series<X, Y> series : getData()) {
-            for (Iterator<Data<X, Y>> it = getDisplayedDataIterator(series); it.hasNext(); ) {
+            Iterator<Data<X, Y>> it = getDisplayedDataIterator(series);
+            while(it.hasNext()) {
                 Data<X, Y> item = it.next();
-                final double x = getXAxis().getDisplayPosition(getCurrentDisplayedXValue(item));
-                final double y = getYAxis().getDisplayPosition(
-                        getYAxis().toRealValue(getYAxis().toNumericValue(getCurrentDisplayedYValue(item))));
+                final double x = xAxis.getDisplayPosition(getCurrentDisplayedXValue(item));
+                final double y = yAxis.getDisplayPosition(
+                        yAxis.toRealValue(yAxis.toNumericValue(getCurrentDisplayedYValue(item))));
                 if (Double.isNaN(x) || Double.isNaN(y)) {
                     continue;
                 }
                 Node symbol = item.getNode();
                 if (symbol != null) {
-                    final double chartYPos = getYAxis().toNumericValue(getYAxis().getValueForDisplay(y));
-                    final double yAxisZeroPos = getYAxis().getZeroPosition();
-                    final boolean isYAxisZeroPosVisible = !Double.isNaN(yAxisZeroPos);
-                    final double yAxisHeight = getYAxis().getHeight();
+                    final double chartYPos = yAxis.toNumericValue(yAxis.getValueForDisplay(y));
                     final double yFillPos = isYAxisZeroPosVisible ? yAxisZeroPos :
                             chartYPos < 0 ? chartYPos - yAxisHeight : yAxisHeight;
-
                     final double w = symbol.prefWidth(-1);
-                    final double h =  Math.sqrt(Math.pow((y - yFillPos), 2));
-
+                    final double h =  Math.abs(y - yFillPos);
                     if (chartYPos > 0) {
                         symbol.resizeRelocate(x-(w/2), y, w, h);
                     } else {
